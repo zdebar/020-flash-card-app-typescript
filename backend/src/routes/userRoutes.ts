32b6b979239011900368c08f66de1594e72f024e
zdebar <157598603+zdebar@users.noreply.github.com>
2025-02-23 07:25:43 +0100

    userRoutes fix

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -8,29 +8,29 @@ const userRoutes = express.Router();
 // Funkce pro registraci uživatele
 userRoutes.post('/register', async (req, res) => {
   const { email, username, password } = req.body;
+  
+  if (!email || !username || !password) {
+    res.status(400).json({ message: 'All fields are required' });
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    res.status(400).json({ message: 'Invalid email format' });
+  }
 
-  // if (!email || !username || !password) {
-  //   return res.status(400).json({ message: 'All fields are required' });
-  // }
-  // if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-  //   return res.status(400).json({ message: 'Invalid email format' });
-  // }
-
-  // try {
-  //   const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
     
-  //   const stmt = db.prepare(`
-  //     INSERT INTO users (email, username, password)
-  //     VALUES (?, ?, ?)
-  //   `);
-  //   const runQuery = promisify(stmt.run.bind(stmt));
-  //   await runQuery(email, username, hashedPassword);
+    const stmt = db.prepare(`
+      INSERT INTO users (email, username, password)
+      VALUES (?, ?, ?)
+    `);
+    const runQuery = promisify(stmt.run.bind(stmt));
+    await runQuery(email, username, hashedPassword);
 
-  //   return res.status(201).json({ message: 'User registered successfully' });
-  // } catch (err) {
-  //   console.error('Error during registration:', err);
-  //   return res.status(500).json({ message: 'Server error during registration' });
-  // }
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    console.error('Error during registration:', err);
+    res.status(500).json({ message: 'Server error during registration' });
+  }
 });
 
 export default userRoutes;
