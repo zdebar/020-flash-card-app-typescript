05c84761c7f1753db3518afe44656cdd9c378023
zdebar <157598603+zdebar@users.noreply.github.com>
2025-09-29 16:52:16 +0200

    chore(clean): delete template files

--- mobile/src/utils/text.utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Provides the correct Czech word for "more" based on the count.
- * @param count
- * @returns
- */
-export function getMoreText(count: number): string {
-  if (count <= 4) return `další`;
-  return `dalších`;
-}
